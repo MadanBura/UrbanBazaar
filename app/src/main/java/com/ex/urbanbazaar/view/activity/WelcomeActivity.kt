03fb4985fba1981d5db3da225419ebb0b0838ee9
Madan Bura <madanbura56@gmail.com>
2025-03-03 10:06:02 +0530

    letsGetStartedScreenFeatureAdded

---
+++ app/src/main/java/com/ex/urbanbazaar/view/activity/WelcomeActivity.kt
@@ -0,0 +1,15 @@
+package com.ex.urbanbazaar.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ex.urbanbazaar.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+
+
+    }
+}
