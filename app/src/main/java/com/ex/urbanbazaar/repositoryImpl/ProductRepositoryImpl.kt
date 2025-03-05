81b9acb9ccf3cb7306d38742ae82aa7e083c303d
Madan Bura <madanbura56@gmail.com>
2025-03-05 12:42:07 +0530

    Added Uncommit Files

--- app/src/main/java/com/ex/urbanbazaar/repositoryImpl/ProductRepositoryImpl.kt
+++ app/src/main/java/com/ex/urbanbazaar/repositoryImpl/ProductRepositoryImpl.kt
@@ -1,4 +1,21 @@
 package com.ex.urbanbazaar.repositoryImpl
 
-class ProductRepositoryImpl {
+import com.ex.urbanbazaar.dataSource.remote.UrbanApiService
+import com.ex.urbanbazaar.model.response.CategoryResponse
+import com.ex.urbanbazaar.repository.ProductRepository
+import retrofit2.Response
+import javax.inject.Inject
+
+class ProductRepositoryImpl @Inject constructor(
+    private val urbanApiService: UrbanApiService
+) : ProductRepository {
+
+    override suspend fun getProductCategories(): Response<CategoryResponse> {
+        val response = urbanApiService.getCategories()
+        return if (response.isSuccessful) {
+            Response.success(response.body())
+        } else {
+            Response.error(response.code(), response.errorBody()!!)
+        }
+    }
 }
